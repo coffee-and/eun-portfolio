d720510bbbdbc13e4ce1ad1d732c155e591cfddf
coffee-and <cheeseee.bagel@gmail.com>
2026-07-26 02:32:01 +0900

    웹 이력서 테마 전환 제거

--- src/components/ResumeSection.tsx
+++ src/components/ResumeSection.tsx
@@ -13,7 +13,6 @@ import "../styles/resume.css";
 import "../styles/resume-icons.css";
 import BrandMark from "./BrandMark";
 import ExternalLink from "./ExternalLink";
-import ThemeToggle from "./ThemeToggle";
 import YouTubeThumbnail from "./YouTubeThumbnail";
 
 const PhotoshopIcon = () => (
@@ -81,12 +80,9 @@ const ResumeSection = () => {
         <a className="resume-page__brand" href="#/" aria-label="portfolio home">
           <BrandMark />
         </a>
-        <div className="resume-page__actions">
-          <ThemeToggle />
-          <a className="resume-page__back" href="#/">
-            back to portfolio
-          </a>
-        </div>
+        <a className="resume-page__back" href="#/">
+          back to portfolio
+        </a>
       </header>
 
       <section className="resume-hero" aria-label="web resume hero">
